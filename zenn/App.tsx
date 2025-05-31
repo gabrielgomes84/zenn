76e56c7f8ceb81a4be7593b7709695196ba4e6a3
Gabriel Gomes <gabegomes2020@gmail.com>
2025-05-31 17:08:05 -0300

    feat: tela login #1

--- zenn/App.tsx
+++ zenn/App.tsx
@@ -1,37 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import LoginScreen from './src/screens/LoginScreen';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Zenn - Gerencie sua rotina! </Text>
-      <TextInput style={styles.input} placeholder="Login" />
-      <TextInput style={styles.input} placeholder="Senha" secureTextEntry />
-      <Button title="Entrar" onPress={() => alert ('Login Pressed')} />
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <LoginScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 16,
-  },
-  title: {
-    fontSize: 24,
-    marginBottom: 20,
-  },
-  input: {
-    width: '80%',
-    height: 40,
-    borderColor: '#ccc',
-    borderWidth: 1,
-    borderRadius: 5,
-    marginBottom: 20,
-    paddingHorizontal: 10,
-  },
-});
+// This is a simple React Native app that imports and displays the LoginScreen component.
