4b3bc6e2fece38b06ebddbd1fdd8686822ad273a
Gabriel Gomes <gabegomes2020@gmail.com>
2025-06-07 22:50:01 -0300

    utilizando dados simulados

--- zenn/src/screens/LoginScreen.tsx
+++ zenn/src/screens/LoginScreen.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
-import { View, TextInput, TouchableOpacity, Text, Image, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, TextInput, TouchableOpacity, Text, Image, StyleSheet, Alert } from 'react-native';
 import ZennIcon from '../assets/zennicon.png';
+import { usuarios, tarefas } from '../data/mockData';
 
 type Props = {
-  navigation: any; // você pode tipar melhor se quiser
+  navigation: any;
 };
 
 export default function LoginScreen({ navigation }: Props) {
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+
+  const handleLogin = () => {
+    const user = usuarios.find(
+    u => u.email.toLowerCase().trim() === email.toLowerCase().trim() && u.senha === senha.trim()
+  );
+    if (user) {
+      navigation.navigate('MainTabs', { screen: 'Tasks', params: { usuarioId: user.id } });
+    } else {
+      Alert.alert('Erro', 'Email ou senha incorretos');
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Image source={ZennIcon} style={styles.logo} />
@@ -16,6 +31,8 @@ export default function LoginScreen({ navigation }: Props) {
         placeholder="Email"
         placeholderTextColor="#EEE9D1"
         keyboardType="email-address"
+        value={email}
+        onChangeText={setEmail}
       />
 
       <TextInput
@@ -23,12 +40,11 @@ export default function LoginScreen({ navigation }: Props) {
         placeholder="Senha"
         placeholderTextColor="#EEE9D1"
         secureTextEntry
+        value={senha}
+        onChangeText={setSenha}
       />
 
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => navigation.navigate('MainTabs', { screen: 'Tasks' })}
-      >
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
         <Text style={styles.buttonText}>Entrar</Text>
       </TouchableOpacity>
 
