9cc2fd48b41f6f760b7c87327f6eb0792fc8a664
Gabriel Gomes <gabegomes2020@gmail.com>
2025-07-06 19:08:50 -0300

    feat: chamar a criação da tabela de usuários assim que o app abrir

--- zenn/App.tsx
+++ zenn/App.tsx
@@ -1,11 +1,16 @@
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './src/AppNavigator';
+import { criarTabelaUsuarios } from './src/database/usuarios';
 
 export default function App() {
+  useEffect(() => {
+    criarTabelaUsuarios();
+  }, []);
+
   return (
     <NavigationContainer>
       <AppNavigator />
     </NavigationContainer>
-
   );
-} // This is the main entry point of the app, where we set up the navigation container and the main navigator.
+}
