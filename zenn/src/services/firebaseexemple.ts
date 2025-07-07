31a8d3d7f36c12fa4c4ed785cc43153fcdacc7e4
Gabriel Gomes <gabegomes2020@gmail.com>
2025-07-07 19:49:33 -0300

    firebase exemplo

---
+++ zenn/src/services/firebaseexemple.ts
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "SUA_API_KEY_AQUI",
+  authDomain: "SEU_AUTH_DOMAIN_AQUI",
+  projectId: "SEU_PROJECT_ID_AQUI",
+  storageBucket: "SEU_STORAGE_BUCKET_AQUI",
+  messagingSenderId: "SEU_MESSAGING_SENDER_ID_AQUI",
+  appId: "SEU_APP_ID_AQUI",
+  measurementId: "SEU_MEASUREMENT_ID_AQUI",
+};
+
+export default firebaseConfig;
